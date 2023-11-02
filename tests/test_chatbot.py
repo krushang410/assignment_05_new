4035d1cbb6ab4ca7e50de1ed90cd54d253d009ea
Krushang Bhatt <Kbhatt3@rrc.ca>
2023-11-02 11:38:42 -0500

    Initial commit: Set up project directory and file structure

--- tests/test_chatbot.py
+++ tests/test_chatbot.py
@@ -1,8 +1,8 @@
 """
-Description:
-Author:
-Date:
-Usage:
+Description: we are going to  practice your knowledge of Python functions by creating a chatbot that assists customers with banking tasks
+Author:Krushang Bhatt
+Date:30/10/2023
+Usage:assists customers with banking tasks.
 """
 import unittest
 from unittest.mock import patch
