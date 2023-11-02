e35940e6ccc2807c466de6ccd317f4fa3b780867
Krushang Bhatt <Kbhatt3@rrc.ca>
2023-11-02 12:19:03 -0500

    Added first trial.

--- tests/test_chatbot.py
+++ tests/test_chatbot.py
@@ -7,3 +7,14 @@ Usage:assists customers with banking tasks.
 import unittest
 from unittest.mock import patch
 
+
+
+#bot-1
+class ChatbotTests(unittest.TestCase):
+    
+    def test_example(self):
+    
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
