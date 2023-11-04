694679725221077d765790ddfa2a80ce1156244d
Krushang Bhatt <Kbhatt3@rrc.ca>
2023-11-04 12:20:52 -0500

    Added 15 tests sucessfully.

--- src/chatbot.py
+++ src/chatbot.py
@@ -2,15 +2,12 @@
 Description: Chatbot application.  Allows user to perform 
 balance inquiries and make deposits to their accounts.
 Author: ACE Department
-Modified by: {Student Name}
-Date: 2023-10-15
+Modified by: {Krushang Bhatt}
+Date: 2023-10-31
 Usage: From the console: python src/chatbot.py
 """
 
 ## GIVEN CONSTANT COLLECTIONS
-from unittest.mock import patch
-
-
 ACCOUNTS = {
     123456 : {"balance" : 1000.0},
     789012 : {"balance" : 2000.0}
@@ -20,107 +17,118 @@ VALID_TASKS = {"balance", "deposit", "exit"}
 
 ## CODE REQUIRED FUNCTIONS STARTING HERE:
 
-
-
-
-## GIVEN CHATBOT FUNCTION
-#01
 def get_account() -> int:
-    
-    while True:
-        try:
-            account_number = int(input("Please enter your account number: "))
-            if account_number in ACCOUNTS:
-                return account_number
-            else:
-                raise Exception("Account number entered does not exist.")
-        except ValueError:
-            raise ValueError("Account number must be a whole number.")
-    
-#02
 
-def get_balance(account : int) -> int :
+    """
+    function does not include parameters and should return an integer
     
-    if account in ACCOUNTS:
-        
-        balance = ACCOUNTS[account]['balance']
-        return f"your current balance for account {account} is $ {balance}" 
-        #print("your current balance for account", account , "is" ,net_balance )
-        
-    else :
-        raise Exception ("Account number does not exist.")
-    
-def get_balance(account : int) -> int :
+    Returns:
+            result(int): Entered account number.
+
+    Raises: 
+           ValueError: Entered a non-numeric value.
+           Exception: Entered invalid account number.
     
     """
-    Retrieve the balance of a specified account.
+    try:
+        acc_number = int(input("Please enter your account number: "))
+    except Exception:
+        raise ValueError("Account number must be a whole number.")
+    if acc_number not in ACCOUNTS:
+        raise Exception("Account number entered does not exist.")
+    return acc_number
+
+def get_amount() -> float:
+    """
+    funtion does not includes any parameters and should return a float.
+
+    Returns:
+            result(float): Entered valid amount.
+
+    Raises:
+            ValueError: If entered amount is negative or invalid.
+            Exception: If invalid amount is entered or less than zero.
+    """
+    try:
+        amount = float(input("Enter the transaction amount:"))
+    except ValueError:
+        raise ValueError("Invalid amount. Amount must be numeric.")
+    if amount <= 0:
+        raise ValueError("Invalid amount. Please enter a positive number.")
+    return amount
+
+def get_balance(account: int) -> str:
+    """
+    retrieve the balance of a particular account.
 
     Args:
-        account : The account number for which to get the balance.
+        account (int): Account number of a person.
 
     Returns:
-        str: A message with account number and its balance.
+        str: Message showing account balance.
 
     Raises:
-        Exception: If the account number is not  in the ACCOUNTS dictionary.
+        Exception: If an account number does not exists in the ACCOUNTS dictionary.
     """
-    
     if account in ACCOUNTS:
-        
-        balance = ACCOUNTS[account]['balance']
-        return f"your current balance for account {account} is $ {balance}" 
-        #print("your current balance for account", account , "is" ,net_balance )
-        
-    else :
-        raise Exception ("Account number does not exist.")
-        
-    
-#print(get_balance(789012))
-
-#3
-def make_deposit(account : int , amount : float ) -> str :
-    
-    
-    """
-    updates balance with deposit from user
-    
-    arg : 
-        account : checks the account number
-        amount: transaction amount to be added in balance
-        
-    Returns:
-        str: a message with deposite amount and corresponding account number
-        
-    Raises:
-        Exception : if account number does not exist
-        Valueerror : if account number entered is less than 0
-    """
-    
-    if account  not in ACCOUNTS :
-        
-        raise Exception("Account number does not exist.")
-    
-    elif amount < 0  :
-        raise ValueError ("Invalid Amount. Amount must be positive.")
-    
+        balance = ACCOUNTS[account] ["balance"]
+        return f"Your current balance for account {account} is ${balance:.2f}"
     else:
-        balance = ACCOUNTS[account]['balance']
-        #new_balance = balance + amount
-        ACCOUNTS[account]["balance"] += amount# updating dict
-        print(ACCOUNTS)
-        #print(new_balance)
-    
-    return  f"You have made a deposite of ${amount} to account {account} ."
-        
-        
-print(ACCOUNTS)
-print(make_deposit(789012,0))
+        raise Exception("Account number does not exist.")
 
+def make_deposit(account: int, amount: float) -> str:
+    """
+    Recieves two parameters.
+    Given two arguments,this function returns a string.
+
+    Args:
+        account(int): Account number to deposit the amount.
+        amount(float): Amount of money deposited.
+
+    Returns:
+        str: Message indicating confirmation of deposit.
+
+    Raises:
+        Exception: If the account number does not exists in the ACCOUNTS dictionary.
+        ValueError: If the amount is not greater than zero. 
+
+    """
+    if account not in ACCOUNTS:
+        raise Exception("Account number does not exist.")
+    if amount <= 0:
+        raise ValueError("Invalid Amount. Amount must be positive.")
+    
+    ACCOUNTS[account]["balance"] += amount
+    return f"You have made a deposit of ${amount} to account {account}"
+   
+def user_selection() -> str:
+    """
+    Return the selection to lowercase.And no parameters are taken.
+
+    Returns:
+        str: User's selected task in lowercase.
+
+    Raises:
+        ValueError: If invalid user selection is provided.
+    """
+    user_choice = input("What would you like to do (balance/deposit/exit)?").lower()
+    if user_choice in VALID_TASKS:
+        return user_choice
+    else:
+        raise ValueError("Invalid task. Please choose balance, deposit, or exit.")
+    
+
+# try:
+#     num = user_selection()
+#     print(num)
+# except ValueError as e:
+#     print(e)
+
+## GIVEN CHATBOT FUNCTION
 ## REQUIRES REVISION
 
-
-"""
 def chatbot():
+    
     '''
     The main program.  Uses the functionality of the functions:
         get_account()
@@ -137,60 +145,55 @@ def chatbot():
         try:
             ## CALL THE user_selection FUNCTION HERE 
             ## CAPTURING THE RESULTS IN A VARIABLE CALLED
-            ## selection:
-
-
+            selection= user_selection()
+            
             if selection != "exit":
-                
+
                 # Account number validation.
                 valid_account = False
                 while valid_account == False:
                     try:
                         ## CALL THE get_account FUNCTION HERE
                         ## CAPTURING THE RESULTS IN A VARIABLE 
-                        ## CALLED account:
+                        account= get_account()
 
 
                         valid_account = True
                     except Exception as e:
                         # Invalid account.
                         print(e)
-                if selection == "balance":
+
+                    if selection == "balance":
                         ## CALL THE get_balance FUNCTION HERE
-                        ## PASSING THE account VARIABLE DEFINED 
-                        ## ABOVE, AND PRINT THE RESULTS:
+                        ## PASSING THE account VARIABLE DEFINED
+                        ## ABOVE, AND PRINT THE RESULT:
+                        balance = get_balance(account)
+                        print (f"Your current balance for account {account} is ${balance}")         
 
-                else:
+                    else:
+                        # Amount validation.
+                        valid_amount = False
+                while valid_amount == False:
+                    try:
+                        ## CALL THE get_amount FUNCTION HERE
+                        ## AND CAPTURE THE RESULTS IN A VARIABLE 
+                        amount= get_amount()
 
-                    # Amount validation.
-                    valid_amount = False
-                    while valid_amount == False:
-                        try:
-                            ## CALL THE get_amount FUNCTION HERE
-                            ## AND CAPTURE THE RESULTS IN A VARIABLE 
-                            ## CALLED amount:
-
-
-                            valid_amount = True
-                        except Exception as e:
-                            # Invalid amount.
-                            print(e)
-                    ## CALL THE make_deposit FUNCTION HERE PASSING THE 
-                    ## VARIABLES account AND amount DEFINED ABOVE AND 
-                    ## PRINT THE RESULTS:
-
+                        valid_amount = True
+                    except Exception as e:
+                        # Invalid amount.
+                        print(e)
+                        ## CALL THE make_deposit FUNCTION HERE PASSING THE 
+                        ## VARIABLES account AND amount DEFINED ABOVE AND 
+                        deposit_output= make_deposit(account, amount)
+                        print(f"You have made a deposit of ${amount} to account {account}")
 
             else:
                 # User selected 'exit'
                 keep_going = False
         except Exception as e:
-            # Invalid selection:
-            print(e)
+                # Invalid selection:
+                print(e)
 
-    print("Thank you for banking with PiXELL River Financial.")
-"""
-    
-"""
 if __name__ == "__main__":
     chatbot()
-"""
--- tests/test_chatbot.py
+++ tests/test_chatbot.py
@@ -1,191 +1,240 @@
 """
-Description: we are going to  practice your knowledge of Python functions by creating a chatbot that assists customers with banking tasks
-Author:Krushang Bhatt
-Date:30/10/2023
-Usage:assists customers with banking tasks.
+Description:Contains test functions and cases to ensures if the function and components are correct.
+Author:Tamanna Singh
+Date:2023-10-30
+Usage:Used to write and run the tests for the functions.
 """
 import unittest
 from unittest.mock import patch
-import sys
+from src.chatbot import get_account
+from src.chatbot import VALID_TASKS, ACCOUNTS
+from src.chatbot import get_amount
+from src.chatbot import get_account, get_amount, get_balance
+from src.chatbot import get_account, get_amount, get_balance, make_deposit
+from src.chatbot import get_account, get_amount, get_balance, make_deposit, user_selection
 
-sys.path.insert(1, "/../assignnment_05/sdf_assignment_5/src")
-from src.chatbot import get_account , get_balance , make_deposit 
-from src.chatbot import VALID_TASKS , ACCOUNTS
-
-
-#test
-class chatbottests_get_account(unittest.TestCase):
-    
-    def test_get_account_numeric(self):
-
- # arrange
-        expected_output= 123456
-        
-        # act
+class ChatbotTests(unittest.TestCase):
+    def test_get_acc_valid_number(self):
         with patch("builtins.input") as mock_input:
+            # Arrange
             mock_input.side_effect = ["123456"]
-        
-        # assert
-        self.assertEqual(expected_output,123456)
+            expected = 123456
 
-    def test_get_account_non_numeric(self):
-        
-        #arrange
+            #Act
+            actual = get_account()
+
+            #Assert
+            self.assertEqual(expected,actual)
+
+    def test_get_account_non_numeric_data(self):
         with patch("builtins.input") as mock_input:
-            mock_input.side_effect = ["non_numeric_data"] 
 
-        #act
-        
-            with self.assertRaises(ValueError) as context:
-                get_account()
-        
-        #assert
-        
-            expected_error = "Account number must be a whole number."
-            self.assertEqual(str(context.exception), expected_error)
+            #Arrange
+            mock_input.side_effect = ["non_numeric_data"]
+            expected = "Account number must be a whole number."
             
-        
-    #Test
-    
-    def test_get_account_not_exist(self):
-        
-        # arrange
-        with patch("builtins.input") as mock_input:
-            mock_input.side_effect = ["112233"]
-        
-        #act
-        
+            #Act
             with self.assertRaises(Exception) as context:
                 get_account()
             
-        #assert
-            expected_error = "Account number entered does not exist."
-            self.assertEqual(str(context.exception), expected_error)
+            #Assert
+            self.assertEqual(str(context.exception), expected)
+        
+    def test_get_account_not_exist(self):
+        with patch("builtins.input") as mock_input:
+
+            #Arrange
+            mock_input.side_effect = ["112233"]
+            expected = "Account number entered does not exist."
+
+            #Act
+            with self.assertRaises(Exception) as context:
+                get_account()
+
+            #Assert
+            self.assertEqual(str(context.exception), expected)
+
+    def test_get_amount_valid_amt(self):
+        with patch("builtins.input") as mock_input:
+
+            #Arrange
+            mock_input.side_effect = ["500.01"]
+            expected = 500.01
+
+            #Act
+            result = get_amount()
+
+            #Assert
+            self.assertEqual(expected, result)
+
+    def test_get_amount_non_numeric_value(self):
+        with patch("builtins.input") as mock_input:
+
+            #Arrange
+            mock_input.side_effect = ["non_numeric_data"]
+            expected = "Invalid amount. Amount must be numeric."
+
+            #Act
+            with self.assertRaises(ValueError) as context:
+                get_amount()
+
+            #Assert
+            self.assertEqual(str(context.exception), expected)
+
+    def test_get_amount_zero_or_negative_amt(self):
+        with patch("builtins.input") as mock_input:
+
+            #Arrange
+            mock_input.side_effect = ["0"] 
+            expected = "Invalid amount. Please enter a positive number."
+
+            #Act
+            with self.assertRaises(ValueError) as context:
+                get_amount()
+
+            #Assert
+            self.assertEqual(str(context.exception), expected)
+
+    def test_get_balance_value_is_correct(self):
+        with patch("builtins.input") as mock_input:
+
+            #Arrange
+            mock_input.side_effect = ["123456"]
+            expected = 1000
+
+            #Assert
+            expected_message = f"Your current balance for account {mock_input.side_effect} is ${expected:.2f}"
+
+    def test_get_balance_account_does_not_exist(self):
+        with patch("builtins.input") as mock_input:
+
+            #Arrange
+            account = ["112233"]
+            expected = "Account number does not exist."
+
+            #Act
+            with self.assertRaises(Exception) as context:
+                get_balance(mock_input.side_effect)
+
+            #Assert
+            self.assertEqual(str(context.exception), expected)
+
+    def test_make_deposit_balance_is_correctly_updated(self):
+
+            #Arrange
+            account = ["123456"]
+            balance = 1000
+            amount = 1500.01
+            expected = 2500.01
+
+            #Act
+            with self.assertRaises(Exception) as context:
+                result = make_deposit()
             
- #2      
-class chatbottests_get_balance(unittest.TestCase):
-    #4
-    def test_get_balance_correct_str_returned(self):
-        
-        #arrange
-        input_value = 123456
-        
-        #act
-        actual_output = get_balance(input_value)
-        
-        expected_output = "your current balance for account 123456 is $ 1000.0"
-        #assert
-        self.assertEqual(expected_output , actual_output)
-    #5  
-    def test_get_balance_exception_raised(self):
-        
-        #arrange
-        input_value = 112233
-        
-        #act
-        with self.assertRaises(Exception) as context:
-            get_balance(input_value)
+            #Assert
+            expected_balance = balance + amount
+            self.assertEqual(expected , expected_balance)
 
-        expected_error_msg ="Account number does not exist."
-        
-        #assert
-        self.assertEqual(str(context.exception), expected_error_msg)
-    
-#3
-
-class chatbottests_make_deposit(unittest.TestCase):
-    #6
-    def test_make_deposite_balance_updated(self):
-        
-        #arrange
-        account_number = 123456
-        ammount_input = 1500
-        
-        #act
-        ACCOUNTS[account_number]["balance"] = 1000.0
-        expected_output = ammount_input + 1000
-        
-        actual_output = make_deposit(123456,1500.0)
-        
-        #assert
-        self.assertEqual( ACCOUNTS[account_number]["balance"], expected_output)
-    #7  
-    def test_make_deposite_str_returned(self):
-        
-        #arrange  
-        account_number = 123456
-        ACCOUNTS[account_number]["balance"] = 1000.
-        input_amount=1000
-        
-        #act
-        expected_output = "You have made a deposite of $1000 to account 123456 ."
-        actual_output = make_deposit(account_number,input_amount)
-        
-        #assert
-        self.assertEqual(expected_output , actual_output)
-    
-    #8
-    def test_make_deposite_exception_error(self):
-      
-        #arrange
-        input_value = 112233
-        input_amount= 1500
-        
-        #act
-        with self.assertRaises(Exception) as context:
-            make_deposit(input_value , input_amount)
-
-        expected_error_msg ="Account number does not exist."
-        
-        #assert
-        self.assertEqual(str(context.exception), expected_error_msg)
-        
-    #9
-    def test_make_deposite_value_error(self):
-      
-        #arrange
-        input_value = 123456
-        input_amount= -50
-        
-        #act
-        with self.assertRaises(ValueError) as context:
-            make_deposit(input_value , input_amount)
-
-        expected_error_msg ="Invalid Amount. Amount must be positive."
-        
-        #assert
-        self.assertEqual(str(context.exception), expected_error_msg)           
+    def test_make_deposit_correct_value_is_returned(self):
             
+            #Arrange
+            account = 123456
+            amount = 1500.01
+
+            #Act
+            result = make_deposit(account, amount)
+
+            #Assert
+            expected = f"You have made a deposit of ${amount} to account {account}"
+            self.assertEqual(result, expected)
+
+    def test_make_deposit_account_does_not_exist(self):
         
+            #Arrnage
+            account = "112233"
+            amount = "1500.01"
+            expected = "Account number does not exist."
+
+            #Act
+            with self.assertRaises(Exception) as context:
+                make_deposit(account, amount)
+
+            #Assert
+            self.assertEqual(str(context.exception), expected)
+
+    def test_make_deposit_amount_is_less_than_zero(self):
+
+            #Arrange
+            account = 123456
+            amount = -50.01
+            
+            #Act
+            with self.assertRaises(ValueError) as context:
+                make_deposit(account, amount)
+
+            #Assert
+            expected = "Invalid Amount. Amount must be positive."
+            self.assertEqual(str(context.exception), expected)
+    
+    def test_user_selection_valid_lowecase_entered(self):
+        with patch("builtins.input") as mock_input:
+
+            #Arrange
+            mock_input.side_effect = ["balance"]
+            expected = "balance"
+
+            #Act
+            actual_output = user_selection()
+
+            #Assert
+            self.assertEqual(expected, actual_output)
+
+    def test_user_selection_valid_uppercase_entered(self):
+        with patch("builtins.input") as mock_input:
+
+            #Arrange
+            mock_input.side_effect = ["BALANCE"]
+            expected = "balance"
+
+            #Act
+            actual_output = user_selection()
+
+            #Assert
+            self.assertEqual(actual_output, expected)
+
+    def test_user_selection_invalid_selection_entered(self):
+        with patch("builtins.input") as mock_input:
+
+            #Arrange
+            mock_input.side_effect = ["invalid_selection"]
+            expected = "Invalid task. Please choose balance, deposit, or exit."
+
+            #Act
+            with self.assertRaises(ValueError) as context:
+                user_selection()
+
+            #Assert
+            self.assertEqual(str(context.exception), expected)
+
+            
+
+
+
+
+
+
+
+
+
+
 
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-if __name__ == '__main__':
-    unittest.main()
-    
+
+
+
+
+
+
 
 
 
